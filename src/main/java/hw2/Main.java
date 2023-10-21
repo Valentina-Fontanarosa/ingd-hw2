b878e6b9e6077e66daa1a73a347a388ba3aa5b4e
Valentina-Fontanarosa <val.fontanarosa2@stud.uniroma3.it>
2023-10-21 13:15:13 +0200

    modifica di main

--- src/main/java/hw2/Main.java
+++ src/main/java/hw2/Main.java
@@ -4,6 +4,6 @@ import java.time.Clock;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cioaosasas");
+        System.out.println("pippo");
     }
 }
